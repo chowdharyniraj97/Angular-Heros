c92d3a2ff74512645795a8837393bdff3c131c29
chowdharyniraj97 <chowdharyniraj97@gmail.com>
2020-05-20 13:07:45 -0400

    added a service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailsComponent } from './hero-details/hero-details.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
   declarations: [
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,16 +9,17 @@ import { HEROES } from '../mock-hero';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes=HEROES;
+  heroes: Hero[];
  selectedHero: Hero;
 
  onClickHero(hero: Hero): void {
   this.selectedHero = hero;
 }
     
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.heroes=this.heroService.getHeroes()
   }
 
 }
